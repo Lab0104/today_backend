da0893b891cc5fff3bce3bca55a53dc233daea1f
HYUNJUN, KIM <denny10002@naver.com>
2023-07-30 23:21:58 +0900

    Keyword 의 DTO, DAO 작성 완료 1. Entity와 dto 간의 데이터 통신 및 2. Service단 구현 3. Controller 작성 필요

---
+++ src/main/java/com/lab0104/todaybackend/data/domain/Keyword.java
@@ -0,0 +1,25 @@
+package com.lab0104.todaybackend.data.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+@Entity(name = "KEYWORD")
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+@Getter
+@Setter
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "keyword_id")
+    private Long id;
+
+    @Column(name = "keyword")
+    private String keyword;
+
+    @Builder
+    public Keyword(Long id, String keyword) {
+        this.id = id;
+        this.keyword = keyword;
+    }
+}
