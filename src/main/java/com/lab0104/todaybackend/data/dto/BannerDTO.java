cb959c913e55345a1eb6740cf92bc1ed475e12bb
김현준 <denny10002@naver.com>
2023-08-31 17:33:14 +0900

    BannerDto 작성중

---
+++ src/main/java/com/lab0104/todaybackend/data/dto/BannerDTO.java
@@ -0,0 +1,41 @@
+package com.lab0104.todaybackend.data.dto;
+
+import com.lab0104.todaybackend.data.domain.Meet;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class BannerDTO {
+
+    @Data
+    @NoArgsConstructor(force = true)
+    @AllArgsConstructor
+    @Builder
+    @ApiModel("BannerInfo")
+    public static class Info {
+
+        private long id;
+        private LocalDateTime display_period;
+        private String title;
+        private String contents;
+        private Long image_url;
+        private String userName;
+        private String meetName;
+    }
+
+    @Data
+    @NoArgsConstructor(force = true)
+    @AllArgsConstructor
+    @Builder
+    @ApiModel("BannerRequest")
+    public static class Request {
+        private LocalDateTime display_period;
+        private String title;
+
+    }
+
+}
